430e9a5beec3b69dc0fd9a59fec7dfa81fcdfb14
Steven Masuch <smasuch@electricmilk.com>
2019-10-02 09:59:13 -0700

    Adding latest test methods

--- share12/ViewController.swift
+++ share12/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var controller: UIDocumentInteractionController?
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func shareIGOfile() {
+        if let URL = Bundle.main.url(forResource: "11378", withExtension: "igo") {
+            controller  = UIDocumentInteractionController(url: URL)
+            controller?.uti = "com.instagram.exclusivegram"
+            controller?.presentOpenInMenu(from: button.frame, in: self.view, animated: true)
+        }
+    }
+    
+    @IBAction func shareIGfile() {
+        if let URL = Bundle.main.url(forResource: "11378", withExtension: "ig") {
+            controller  = UIDocumentInteractionController(url: URL)
+            controller?.uti = "com.instagram.photo"
+            controller?.presentOpenInMenu(from: button.frame, in: self.view, animated: true)
+        }
+    }
+    
+    @IBAction func shareJPEGfile() {
+        if let URL = Bundle.main.url(forResource: "IMG_4866", withExtension: "jpeg") {
+            controller  = UIDocumentInteractionController(url: URL)
+            controller?.uti = "public.jpeg"
+            controller?.presentOpenInMenu(from: button.frame, in: self.view, animated: true)
+        }
+    }
 }
-
